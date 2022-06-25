6d6b4dd91b6faf1345faa6a17d26a9e14e5118d3
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-24 22:30:43 -0700

    test 4 = green

--- __tests__/planet.tests.js
+++ __tests__/planet.tests.js
@@ -2,24 +2,24 @@ import Ageonotherplanets from './../src/planets.js';
 
 describe('Ageonotherplanets', () => {
 
-  test('test1: should return an object called age with one property: earth age)', () => {
+  test('test 1: should return an object called age with one property: earth age)', () => {
     const user = new Ageonotherplanets(100);
     expect(user.earthAge).toEqual(100);
   });
 
-  test('test2: should return their age in Mercury years. (A Mercury year is .24 Earth years.)', () => {
+  test('test 2: should return their age in Mercury years. (A Mercury year is .24 Earth years.)', () => {
     const user = new Ageonotherplanets(100);
     expect(user.getMercuryAge()).toEqual(24);
   });
 
-  test('test3: should return their age in Venus years. (A Venus year is .62 Earth years.)', () => {
+  test('test 3: should return their age in Venus years. (A Venus year is .62 Earth years.)', () => {
     const user = new Ageonotherplanets(100);
     expect(user.getVenusAge()).toEqual(62);
   });
 
-  test('should return their age in Mars years. (A Mars year is 1.88 Earth years.)', () => {
+  test('test 4: should return their age in Mars years. (A Mars year is 1.88 Earth years.)', () => {
     const user = new Ageonotherplanets(100);
-    expect(user.getMarsAge()).toEqual(62);
+    expect(user.getMarsAge()).toEqual(188);
   });
 
   // test('should return their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)', () => {
