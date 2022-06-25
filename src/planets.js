075c97edda966c4d74dc17393712052225343ff8
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-24 19:51:29 -0700

    export default class Ageonotherplanets class for the other planets and added some meathods to get those ages

--- src/planets.js
+++ src/planets.js
@@ -2,7 +2,19 @@ export default class Ageonotherplanets {
   constructor(age) {
     this.earthAge = age;
   }
-  
+  mercuryAge = 0;
+  venusAge = 0;
+  marsAge = 0;
+  jupiterAge = 0;
+
+  getMercuryAge() {
+  }
+  getVenusAge() {
+  }
+  getMarsAge() {
+  }
+  getJupiterAge() {
+  }
 }
 
-//module.exports = Ageonotherplanets;
+
