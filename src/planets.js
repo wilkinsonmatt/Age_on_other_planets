c2724415a0440dcdb465f535eaa4f05b4661e16c
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-26 18:28:18 -0700

    added protetyies to class

--- src/planets.js
+++ src/planets.js
@@ -1,28 +1,42 @@
 export default class Ageonotherplanets {
   constructor(age) {
-    this.earthAge = age;
+    this.age = age;
+    this.lifeExpectancy = 68;
+    this.yearsToLive = this.lifeExpectancy - this.age;
+    this.yearsToLiveMercury = this.yearsToLive * .24;
   }
-
-
-
+  
   getMercuryAge() {
-    let mercuryAge = .24 * this.earthAge;
-    return mercuryAge;
+    return .24 * this.age;
   }
   getVenusAge() {
-    let venusAge = .62 * this.earthAge;
-    return venusAge;
+    return .62 * this.age;
   }
   getMarsAge() {
-    let marsAge = 1.88 * this.earthAge;
-    return marsAge;
+    return 1.88 * this.age;
   }
   getJupiterAge() {
-    let jupiterAge = 11.86 * this.earthAge;
-    return jupiterAge;
+    return 11.86 * this.age;
   }
-  getsListOfYearsToLiveOnOtherPlanets() {
+
+
+
+  getListOfLifeExpectancyforEachPlanet() {
+    let list = [];
+    const lifeExpectancy = 68;
+    if(this.age > lifeExpectancy){
+      list.push(`You've lived ${(this.age - lifeExpectancy)} Earth years past the average life expectancy`);
+      list.push(`You've lived ${(this.age - lifeExpectancy) * .24} Mercury years past the average life expectancy`);
+      list.push(`You've lived ${(this.age - lifeExpectancy) * .62} Venus years past the average life expectancy`);
+      list.push(`You've lived ${(this.age - lifeExpectancy) * 1.88} Mars years past the average life expectancy`);
+      list.push(`You've lived ${(this.age - lifeExpectancy) * 11.86} Jupiter years past the average life expectancy`);
+    }else{
+      list.push(`Based on average life expectancy you have ${(lifeExpectancy - this.age)} Earth years left to live`);
+      list.push(`Based on average life expectancy you have ${(lifeExpectancy - this.age) * .24} Mercury years left to live`);
+      list.push(`Based on average life expectancy you have ${(lifeExpectancy - this.age) * .62} Venus years left to live`);
+      list.push(`Based on average life expectancy you have ${(lifeExpectancy - this.age) * 1.88} Mars years left to live`);
+      list.push(`Based on average life expectancy you have ${(lifeExpectancy - this.age) * 11.86} Jupiter years left to live`);
+    }
+    return list;
   }
 }
-
-
