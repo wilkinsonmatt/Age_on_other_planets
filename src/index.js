c876a1139b98fd7eab1bbcc48097fd459d86cf1b
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-26 21:47:57 -0700

    update basic UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import Ageonotherplanets from './planets.js';
+import { Ageonotherplanets, checkAge }  from './../src/planets.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
+
 $('#age-form').submit(function(event) {
   event.preventDefault();
   let age = parseInt($('#length1').val());
-  let user = new Ageonotherplanets(age);
-  console.log(user.getMercuryAge());
-  $('#response').append("<p>" + user.earthAge + "</p>");
+  if(checkAge){
+    let user = new Ageonotherplanets(age);
+    $('#response').append("<p>" + user.earthAge + "</p>");
+  }else{
+    $('#response').append("You can't be negative years old silly");
+  }
 }); 
+
+
