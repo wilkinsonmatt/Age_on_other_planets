53e182e9e4f887b2ecded59f652023425d761f76
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-06-26 20:45:36 -0700

    test 13 and 14 = red

--- __tests__/planet.tests.js
+++ __tests__/planet.tests.js
@@ -57,6 +57,22 @@ describe('Ageonotherplanets', () => {
     expect(user.getNumberOfYearsUserHasLivePastLifeExpectany()).toEqual(0);
   });
 
+  test('test 12: should array of strings that ', () => {
+    const user = new Ageonotherplanets(26);
+    expect(user.getNumberOfYearsUserHasLivePastLifeExpectany()).toEqual(0);
+  });
+
+  test('test 13: should return an array containing how many planet years the user has to live on each planet', () => {
+    const user = new Ageonotherplanets(100);
+    expect(user.getListOfLifeExpectancyforEachPlanet()).toEqual(["You've lived 32 Earth years past the average life expectancy", "You've lived 7.68 Mercury years past the average life expectancy", "You've lived 19.84 Venus years past the average life expectancy", "You've lived 60.16 Mars years past the average life expectancy", "You've lived 379.52 Jupiter years past the average life expectancy"]);
+  });
+
+  test('test 13: should return an array containing how many planet years the user has lived past the life expectancy on each planet', () => {
+    const user = new Ageonotherplanets(26);
+    expect(user.getListOfLifeExpectancyforEachPlanet()).toEqual(["Based on average life expectancy you have 42 Earth years left to live", "Based on average life expectancy you have 10.08 Mercury years left to live", "Based on average life expectancy you have 26.04 Venus years left to live", "Based on average life expectancy you have 78.96 Mars years left to live", "Based on average life expectancy you have 498.12 Jupiter years left to live"]);
+  });
+  
+
 });
 
 
